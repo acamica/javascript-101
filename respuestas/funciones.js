325d24a9a1373d572c010d5536277816d7704550
Francisco Dorr <fdorr@dc.uba.ar>
2017-08-17 10:31:10 -0300

    Update funciones.js

--- respuestas/funciones.js
+++ respuestas/funciones.js
@@ -1,4 +1,3 @@
-```javascript
 var sumar = function(num1, num2) {
     return num1 + num2;
 };
@@ -16,4 +15,4 @@ console.log("25 + 3 es " + sumar(25, 3));
 console.log("142 - 8 es " + restar(142, 8));
 console.log("6 * 7 es " + multiplicar(6, 7));
 console.log("40 / 8 es " + dividir(40, 8));
-```
+
