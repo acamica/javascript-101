1d79fbfa56061ccc3759f8bc9f25b2c38e94b159
Francisco Dorr <fdorr@dc.uba.ar>
2017-08-17 10:55:49 -0300

    Create bucles.js

---
+++ respuestas/bucles.js
@@ -0,0 +1,23 @@
+var esPar = function(nro) {
+  return (nro % 2) === 0;
+}
+
+for(var i = 1; i <= 40; i++){
+  if(esPar(i)){
+    console.log(i);
+  }
+}
+
+
+var grillaNumeros = [
+  [1, 5, 10],
+  [5, 20, 22],
+  [30, 17, 46]
+]
+
+for (var i = 0; i < grillaNumeros.length; i++) {
+  for (var j = 0; j < grillaNumeros.length; j++) {
+    var actual = grillaNumeros[i][j];
+    console.log(actual);
+  }
+}
